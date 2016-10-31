4edd505abbb63c130c20723f6d084ba18175cdec
Александр <alex_eternity@mail.ru>
2016-10-31 09:52:18 +0700

    fdsfds
    fsd

--- lab1.py
+++ lab1.py
@@ -6,7 +6,7 @@ url=input();
 if not url.startswith('http://www.'):
 	url="http://www." + url
 print(url)
-print('enter number of ogranichitel(number or 0 for all)');
+print('enter number of ogranichitel(number)');
 global k
 k=input();
 k=int(k);									
@@ -15,7 +15,6 @@ ssilki=set();
 global glub
 glub=0
 def recursion(j):
-	if k != 0:
 		global glub,url,k
 		glub += 1
 		r=requests.get(j)		
@@ -32,23 +31,5 @@ def recursion(j):
 		if glub<k:
 			for i in l[0:min(k,len(l))]:
 				recursion(url+i)
-	else:
-		global url,k
-		r=requests.get(j)		
-		res=r.text;
-		result=re.findall( '[\w\.-]+@[\w\.-]+',res);	
-		for email in result:
-			mails.add(email)
-		l=re.findall('<a\s+href="(/(?!:)[^"#]+)',res);
-		for link in l:
-			if link not in ssilki:
-				ssilki.add(link)
-			else:
-				l.remove(link)
-		if l != []:
-			for i in l[0:len(l)]:
-				recursion(url+i)
-		else:
-			return
 recursion(url)
 print(mails);
